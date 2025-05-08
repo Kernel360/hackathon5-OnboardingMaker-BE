59115fdeeb657ac9f0ee5061865745b49811ffce
Clsz <45088680+ClszK@users.noreply.github.com>
2025-05-09 07:31:25 +0900

    Fix: Request 접두사 /api 추가

--- src/main/java/com/example/onboarding/controller/MissionController.java
+++ src/main/java/com/example/onboarding/controller/MissionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/mission")
+@RequestMapping("/api/mission")
 public class MissionController {
 
     private final MissionService missionService;
--- src/main/java/com/example/onboarding/controller/MissionWriteController.java
+++ src/main/java/com/example/onboarding/controller/MissionWriteController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/missionWrite")
+@RequestMapping("/api/missionWrite")
 public class MissionWriteController {
 
     private final GlobalExceptionHandler globalExceptionHandler;

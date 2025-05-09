61ad0d0f90b4ca30c859f58d4207ff4ae5abad31
sujeong <thehapy0103@naver.com>
2025-05-09 09:00:05 +0900

    getAllMissions 필드 추가

--- src/main/java/com/example/onboarding/dto/MissionListDto.java
+++ src/main/java/com/example/onboarding/dto/MissionListDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class MissionListDto {
     private String title;
     private String description;
-    private LocalDateTime deadline;
+    private LocalDateTime deadlWine;
+    private Integer missionId;
 
 }
--- src/main/java/com/example/onboarding/repository/MissionRepository.java
+++ src/main/java/com/example/onboarding/repository/MissionRepository.java
@@ -11,7 +11,7 @@ public interface MissionRepository extends JpaRepository<Mission, Integer> {
 
     @Query("""
     SELECT new com.example.onboarding.dto.MissionListDto(
-          m.title, m.description, m.deadline
+          m.title, m.description, m.deadline, m.missionId
         )
     FROM Mission m
     ORDER BY m.deadline ASC
